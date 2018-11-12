e9c0d2bb89d918f702c9c3c5afe4667964e35a1b
newtonapple <david.github@gmail.com>
2018-11-12 02:01:45 -0800

    Refactored newtonapple's xd75 layout to use the newtonapple user profile.

--- keyboards/xd75/keymaps/newtonapple/rules.mk
+++ keyboards/xd75/keymaps/newtonapple/rules.mk
@@ -1,4 +1,4 @@
-# Copyright 2013 Jun Wako <wakojun@gmail.com>
+# Copyright 2018 David Dai <newtonapple>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
-
+BOOTMAGIC_ENABLE = no
+COMMAND_ENABLE = no
 # RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
