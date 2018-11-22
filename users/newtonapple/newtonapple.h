b3dd476ebb155157b576b8c17c45797e3af9c124
newtonapple <david.github@gmail.com>
2018-11-21 18:13:10 -0800

    WIP: Reworked yanking.

--- users/newtonapple/newtonapple.h
+++ users/newtonapple/newtonapple.h
@@ -7,12 +7,13 @@
 enum newtonapple_layers {
   _QWERTY,
   _CAP,
-  _NUM,
-  _SYM,
-  _MOUSE,
 #ifdef ENABLE_MACVIM
   _MACVIM,
+  _VIMNUM,
 #endif
+  _MOUSE,
+  _NUM,
+  _SYM,
 };
 
 #define QWERTY TO(_QWERTY)
@@ -27,8 +28,11 @@ enum newtonapple_layers {
 #define MOUSE_GRV LT(_MOUSE, KC_GRV)
 #define MOUSE_TAB LT(_MOUSE, KC_TAB)
 #ifdef ENABLE_MACVIM
+#define VIMNUM TO(_VIMNUM)
 #define MACVIM_ESC LT(_MACVIM, KC_ESC)
 #define MACVIM_SPC LT(_MACVIM, KC_SPC)
+#define MO_VIMNUM MO(_VIMNUM)
+#define TG_VIMNUM TG(_VIMNUM)
 #endif
 
 #define SYM_MINS LT(_SYM, KC_MINS)
@@ -36,5 +40,9 @@ enum newtonapple_layers {
 #define NUM_MINS LT(_NUM, KC_MINS)
 #define GUI_T_EQL GUI_T(KC_EQL)
 #define SFT_T_QUOT SFT_T(KC_QUOT)
+
 #define OSL_NUM OSL(_NUM)
 #define OSL_SYM OSL(_SYM)
+
+#define TT_NUM TT(_NUM)
+#define TT_SYM TT(_SYM)
