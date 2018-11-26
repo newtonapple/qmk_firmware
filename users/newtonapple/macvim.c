3859f48b3c497b736a623f2679bafc665f554389
newtonapple <david.github@gmail.com>
2018-11-26 05:20:15 -0800

    WIP: refactored yanking logics to use macros.

--- users/newtonapple/macvim.c
+++ users/newtonapple/macvim.c
@@ -2,105 +2,135 @@
 
 #define VIM_CUT_NEXT_CHAR()                                                    \
   MAC_CUT_NEXT_CHAR(VIM_REPEAT())                                              \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_CUT_PREV_CHAR()                                                    \
-  VIM_WITH_MOD(LSHIFT, MAC_CUT_PREV_CHAR(VIM_REPEAT()))
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_CUT_PREV_CHAR(VIM_REPEAT()))
 
 #define VIM_CUT_TO_START_OF_WORD_FROM_CURSOR()                                 \
-  VIM_WITH_MOD(LCTRL, MAC_CUT_TO_START_OF_WORD_FROM_CURSOR(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(                                            \
+      LCTRL, MAC_CUT_TO_START_OF_WORD_FROM_CURSOR(VIM_REPEAT()));
 
 #define VIM_CUT_TO_END_OF_WORD_FROM_CURSOR()                                   \
   MAC_CUT_TO_END_OF_WORD_FROM_CURSOR(VIM_REPEAT())                             \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_CUT_WORD_ON_CURSOR()                                               \
   MAC_CUT_WORD_ON_CURSOR(VIM_REPEAT())                                         \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_CUT_TO_START_OF_LINE()                                             \
   MAC_SEL_TO_START_OF_LINE()                                                   \
   MAC_CUT()                                                                    \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_CUT_TO_END_OF_LINE()                                               \
   MAC_SEL_TO_END_OF_LINE()                                                     \
   MAC_CUT()                                                                    \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_CUT_LINE()                                                         \
   MAC_CUT_LINE(VIM_REPEAT())                                                   \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
-#define VIM_COPY_LINE()                                                        \
+/*
+ * Used only when visual mode AND shifts keys are off.
+ * It copies up to number of lines specified in vim_repeat and also resets
+ * the vimode and the vim_repeat counter.
+ */
+#define VIM_YANK_LINES()                                                       \
+  CLEAR_VIMODE(YANK)                                                           \
   MAC_COPY_LINE(VIM_REPEAT())                                                  \
   MAC_LEFT();                                                                  \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
+
+/*
+ * Used only in conjunction with visual mode being on.
+ * It copies everything that is currently visually selected.
+ */
+#define VIM_VISUAL_YANK()                                                      \
+  MAC_UP_SHIFT();                                                              \
+  MAC_COPY();                                                                  \
+  MAC_LEFT();                                                                  \
+  reset_macvim_states();                                                       \
+  return false;
+
+#define VIM_SHIFT_YANK() VIM_EXEC_IF_SHIFT(MAC_COPY());
 
 #define VIM_SEL_NEXT_CHAR()                                                    \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_NEXT_CHAR(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_NEXT_CHAR(VIM_REPEAT()));
 
 #define VIM_SEL_PREV_CHAR()                                                    \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_PREV_CHAR(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_PREV_CHAR(VIM_REPEAT()));
 
 #define VIM_SEL_TO_START_OF_WORD()                                             \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_START_OF_WORD(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT,                                     \
+                                   MAC_SEL_TO_START_OF_WORD(VIM_REPEAT()));
 
 #define VIM_SEL_TO_END_OF_WORD()                                               \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_END_OF_WORD(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT,                                     \
+                                   MAC_SEL_TO_END_OF_WORD(VIM_REPEAT()));
 
 #define VIM_SEL_TO_START_OF_LINE()                                             \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_START_OF_LINE());
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_TO_START_OF_LINE());
 
-#define VIM_SEL_TO_END_OF_LINE() VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_END_OF_LINE());
+#define VIM_SEL_TO_END_OF_LINE()                                               \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_TO_END_OF_LINE());
 
-#define VIM_SEL_LINE() VIM_WITH_MOD(LSHIFT, MAC_SEL_LINE(VIM_REPEAT()));
+#define VIM_SEL_LINE()                                                         \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_LINE(VIM_REPEAT()));
 
 #define VIM_SEL_TO_NEXT_LINE()                                                 \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_NEXT_LINE(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_TO_NEXT_LINE(VIM_REPEAT()));
 
 #define VIM_SEL_TO_PREV_LINE()                                                 \
-  VIM_WITH_MOD(LSHIFT, MAC_SEL_TO_PREV_LINE(VIM_REPEAT()));
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, MAC_SEL_TO_PREV_LINE(VIM_REPEAT()));
 
 #define VIM_MOV_TO_NEXT_CHAR()                                                 \
   MAC_MOV_TO_NEXT_CHAR(VIM_REPEAT())                                           \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_MOV_TO_PREV_CHAR()                                                 \
   MAC_MOV_TO_PREV_CHAR(VIM_REPEAT())                                           \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_MOV_TO_START_OF_WORD()                                             \
   MAC_MOV_TO_START_OF_WORD(VIM_REPEAT())                                       \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_MOV_TO_END_OF_WORD()                                               \
   MAC_MOV_TO_END_OF_WORD(VIM_REPEAT())                                         \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_MOV_TO_NEXT_LINE()                                                 \
   MAC_MOV_TO_NEXT_LINE(VIM_REPEAT())                                           \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
 #define VIM_MOV_TO_PREV_LINE()                                                 \
   MAC_MOV_TO_PREV_LINE(VIM_REPEAT())                                           \
-  CLEAR_VIM_REPEAT_AND_RETURN()
+  RESET_VIM_REPEAT()
 
-#define VIM_MOV_LINE_UP() VIM_WITH_MOD(LALT, MAC_MOV_LINE_UP(VIM_REPEAT()));
+#define VIM_MOV_LINE_UP()                                                      \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LALT, MAC_MOV_LINE_UP(VIM_REPEAT()));
 
-#define VIM_MOV_LINE_DOWN() VIM_WITH_MOD(LALT, MAC_MOV_LINE_DOWN(VIM_REPEAT()));
+#define VIM_MOV_LINE_DOWN()                                                    \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LALT, MAC_MOV_LINE_DOWN(VIM_REPEAT()));
 
-#define VIM_WITH_MOD(MOD, CODE)                                                \
+#define VIM_EXEC_AND_RESET_REPEAT_IF_MOD(MOD, CODE)                            \
   if (IS_MOD_ON(MOD)) {                                                        \
     SEND_STRING(SS_UP(X_##MOD));                                               \
     CODE;                                                                      \
     SEND_STRING(SS_DOWN(X_##MOD));                                             \
-    CLEAR_VIM_REPEAT_AND_RETURN();                                             \
+    RESET_VIM_REPEAT();                                                        \
   }
+
+#define VIM_EXEC_IF_SHIFT(CODE)                                                \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(LSHIFT, CODE);                              \
+  VIM_EXEC_AND_RESET_REPEAT_IF_MOD(RSHIFT, CODE);
 
 #define IS_MOD_ON(MOD) get_mods() & MOD_BIT(KC_##MOD)
 #define VIM_REPEAT() get_repeat()
-#define CLEAR_VIM_REPEAT_AND_RETURN()                                          \
+#define RESET_VIM_REPEAT()                                                     \
   vim_repeat = 0;                                                              \
   return false;
 
@@ -201,25 +231,17 @@ bool process_macvim(uint16_t keycode, keyrecord_t *record, bool with_repeat) {
       }
       return false;
     case VIM_Y:
+      // yank while in visual mode (vy)
       if (IS_VIMODE(VISUAL)) {
-        // don't hold down shift while copy
-        MAC_UP_SHIFT();
-        MAC_COPY();
-        // deselect by moving one character to the left
-        MAC_LEFT();
-        reset_macvim_states();
-        return false;
+        VIM_VISUAL_YANK();
       }
-      // allow copy while holding down shift
-      VIM_WITH_MOD(LSHIFT, MAC_COPY());
-      VIM_WITH_MOD(RSHIFT, MAC_COPY());
-
+      // allow yanking while holding down shift and not in visual mode
+      VIM_SHIFT_YANK();
+      // yanking multiple lines (yy)
       if (IS_VIMODE(YANK)) {
-        CLEAR_VIMODE(YANK);
-        VIM_COPY_LINE();
-      } else {
-        SET_VIMODE_YANK();
+        VIM_YANK_LINES();
       }
+      SET_VIMODE_YANK();
       return false;
     case VIM_X:
       VIM_CUT_PREV_CHAR();
