01265f1ee550e3ad6c5a315a5bfa3b08d558398c
David Dai <ddai@scribd.com>
2019-02-24 19:04:31 -0800

    Preonic: use space key to trigge mouse layer.

--- keyboards/preonic/keymaps/newtonapple/keymap.c
+++ keyboards/preonic/keymaps/newtonapple/keymap.c
@@ -20,11 +20,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_preonic_grid(
-    MOUSE_GRV,  KC_1,    KC_2,       KC_3,      KC_4,      KC_5,       KC_6,         KC_7,       KC_8,     KC_9,       KC_0,     KC_BSPC,
-    MOUSE_TAB,  KC_Q,    KC_W,       KC_E,      KC_R,      KC_T,       KC_Y,         KC_U,       KC_I,     KC_O,       KC_P,     KC_BSPC,
-    MACVIM_ESC, KC_A,    KC_S,       KC_D,      KC_F,      KC_G,       KC_H,         KC_J,       KC_K,     KC_L,       KC_SCLN,  KC_ENT,
-    KC_LSPO,    KC_Z,    KC_X,       KC_C,      KC_V,      KC_B,       KC_N,         KC_M,       KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSPC,
-    CRTL_SLBRC, KC_LCTL, ALT_T_LBRC, GUI_T_EQL, SYM_SMINS, MACVIM_SPC, MACVIM_SQUOT, SFT_T_QUOT, NUM_MINS, GUI_T_RBRC, ADJ_BSLS, CRTL_SRBRC
+    MOUSE_GRV,  KC_1,    KC_2,       KC_3,      KC_4,      KC_5,      KC_6,         KC_7,       KC_8,     KC_9,       KC_0,     KC_BSPC,
+    MOUSE_TAB,  KC_Q,    KC_W,       KC_E,      KC_R,      KC_T,      KC_Y,         KC_U,       KC_I,     KC_O,       KC_P,     KC_BSPC,
+    MACVIM_ESC, KC_A,    KC_S,       KC_D,      KC_F,      KC_G,      KC_H,         KC_J,       KC_K,     KC_L,       KC_SCLN,  KC_ENT,
+    KC_LSPO,    KC_Z,    KC_X,       KC_C,      KC_V,      KC_B,      KC_N,         KC_M,       KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSPC,
+    CRTL_SLBRC, KC_LCTL, ALT_T_LBRC, GUI_T_EQL, SYM_SMINS, MOUSE_SPC, MACVIM_SQUOT, SFT_T_QUOT, NUM_MINS, GUI_T_RBRC, ADJ_BSLS, CRTL_SRBRC
   ),
 
   [_CAP] = LAYOUT_preonic_grid(
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_MACVIM] = LAYOUT_preonic_grid(
-    VIM_ESC,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     KC_6,      KC_7,     KC_8,      KC_9,    KC_0,    _______,
+    VIM_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,      KC_6,      KC_7,     KC_8,      KC_9,    KC_0,    _______,
     _______,   VIM_0,   VIM_W,   VIM_E,   REDO,     VIM_DOL,   VIM_Y,     UNDO,     VIM_I,     VIM_O,   PASTE,   CUT,
     DEFAULT,   SEL_ALL, SAVE,    VIM_D,   CMD_LEFT, CMD_RGHT,  KC_LEFT,   KC_DOWN,  KC_UP,     KC_RGHT, COPY,    _______,
     TG_VIMNUM, UNDO,    VIM_X,   VIM_C,   VIM_V,    VIM_B,     PREV_TAB,  NEXT_TAB, UNINDENT,  INDENT,  _______, _______,
-    _______,   _______, _______, _______, KC_LSFT,  _______,   KC_ENTER,   KC_LSFT,  MO_VIMNUM, _______, _______, _______
+    _______,   _______, _______, _______, KC_LSFT,  _______,   KC_ENTER,  KC_LSFT,  MO_VIMNUM, _______, _______, _______
   ),
 
   [_VIMNUM] = LAYOUT_preonic_grid(
@@ -52,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_MOUSE] = LAYOUT_preonic_grid(
-    _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, _______,  _______,  _______, _______,
-    _______, QUIT,    CLOSE,   KC_ACL2, REDO,    KC_PGUP, COPY,    UNDO,    PREV_TAB, NEXT_TAB, PASTE,   CUT,
-    DEFAULT, SEL_ALL, KC_ACL0, KC_ACL1, KC_ACL2, KC_PGDN, KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
-    _______, _______, _______, _______, KC_LSFT, _______, KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R,  KC_UP,   KC_UP,
-    _______, _______, _______, _______, KC_LSFT, KC_ACL1, KC_ACL0, KC_BTN1, KC_BTN2,  KC_LEFT,  KC_DOWN, KC_RGHT
+    _______, KC_ACL2, KC_ACL0, KC_ACL1, _______, _______, _______,  _______, _______,  _______,  _______, _______,
+    _______, QUIT,    CLOSE,   KC_ACL2, REDO,    KC_PGUP, COPY,     UNDO,    PREV_TAB, NEXT_TAB, PASTE,   CUT,
+    DEFAULT, SEL_ALL, KC_ACL2, KC_ACL0, KC_ACL1, KC_PGDN, KC_MS_L,  KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
+    _______, _______, _______, _______, KC_LSFT, _______, KC_WH_L,  KC_WH_D, KC_WH_U,  KC_WH_R,  KC_UP,   KC_UP,
+    _______, _______, _______, _______, KC_LSFT, KC_ACL1, KC_ENTER, KC_BTN1, KC_BTN2,  KC_LEFT,  KC_DOWN, KC_RGHT
   ),
 
   [_NUM] = LAYOUT_preonic_grid(
