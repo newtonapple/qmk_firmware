1a7240a1fa30339278f35cda798e3453be25520b
newtonapple <david.github@gmail.com>
2019-03-05 00:43:27 -0800

    kbd4x keymaps update.

--- keyboards/kbdfans/kbd4x/keymaps/newtonapple/keymap.c
+++ keyboards/kbdfans/kbd4x/keymaps/newtonapple/keymap.c
@@ -18,10 +18,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_planck_mit(
-    MOUSE_TAB,  KC_Q,    KC_W,       KC_E,      KC_R,      KC_T,       KC_Y, KC_U,       KC_I,     KC_O,      KC_P,     KC_BSPC,
-    MACVIM_ESC, KC_A,    KC_S,       KC_D,      KC_F,      KC_G,       KC_H, KC_J,       KC_K,     KC_L,      KC_SCLN,  KC_ENT,
-    KC_LSPO,    KC_Z,    KC_X,       KC_C,      KC_V,      KC_B,       KC_N, KC_M,       KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSPC,
-    CRTL_SLBRC, KC_LCTL, ALT_T_LBRC, GUI_T_EQL, SYM_SQUOT, MACVIM_SPC,       SFT_T_QUOT, NUM_MINS, CMD_SMINS, LED_RBRC, CRTL_SRBRC
+    MOUSE_TAB,  KC_Q,    KC_W,       KC_E,      KC_R,      KC_T,       KC_Y, KC_U,       KC_I,     KC_O,       KC_P,     KC_BSPC,
+    MACVIM_ESC, KC_A,    KC_S,       KC_D,      KC_F,      KC_G,       KC_H, KC_J,       KC_K,     KC_L,       KC_SCLN,  KC_ENT,
+    KC_LSPO,    KC_Z,    KC_X,       KC_C,      KC_V,      KC_B,       KC_N, KC_M,       KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSPC,
+    CRTL_SLBRC, KC_LCTL, ALT_T_LBRC, GUI_T_EQL, SYM_SQUOT, MACVIM_SPC,       SFT_T_QUOT, NUM_MINS, GUI_T_RBRC, LED_RBRC, CRTL_SRBRC
   ),
 
   [_CAP] = LAYOUT_planck_mit(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOUSE] = LAYOUT_planck_mit(
     _______, QUIT,    CLOSE,   KC_ACL2, REDO,    KC_PGUP, COPY,    UNDO,    PREV_TAB, NEXT_TAB, PASTE,   CUT,
-    DEFAULT, SEL_ALL, KC_ACL0, KC_ACL1, KC_ACL2, KC_PGDN, KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
+    DEFAULT, SEL_ALL, KC_ACL2, KC_ACL0, KC_ACL1, KC_PGDN, KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
     _______, _______, _______, _______, KC_LSFT, _______, KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R,  KC_UP,   KC_UP,
     _______, _______, _______, _______, KC_LSFT,          KC_ACL0, KC_BTN1, KC_BTN2,  KC_LEFT,  KC_DOWN, KC_RGHT
   ),
