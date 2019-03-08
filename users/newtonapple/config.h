8344efb54bdd1b3a52e9fc286c15423b70db9618
newtonapple <david.github@gmail.com>
2019-03-08 02:44:20 -0800

    Speed up mouse & move comment configs to newtonapple user namespace.

--- users/newtonapple/config.h
+++ users/newtonapple/config.h
@@ -1,13 +1,23 @@
 
 #pragma once
 
-#define MOUSEKEY_DELAY             25  // default 300
-#define MOUSEKEY_INTERVAL          35  // default 50
+// #define MOUSEKEY_DELAY             25  // default 300
+// #define MOUSEKEY_INTERVAL          35  // default 50
+// #define MOUSEKEY_MAX_SPEED         20  // default 10
+// #define MOUSEKEY_TIME_TO_MAX       20  // default 20
+// #define MOUSEKEY_WHEEL_MAX_SPEED   18  // default 8
+// #define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // default 40
+
+#define MOUSEKEY_DELAY              0  // default 300
+#define MOUSEKEY_INTERVAL          16  // default 50
 #define MOUSEKEY_MAX_SPEED         20  // default 10
-#define MOUSEKEY_TIME_TO_MAX       20  // default 20
-#define MOUSEKEY_WHEEL_MAX_SPEED   18  // default 8
+#define MOUSEKEY_TIME_TO_MAX       85  // default 20
+// #define MOUSEKEY_WHEEL_DELAY        0
+#define MOUSEKEY_WHEEL_MAX_SPEED   15  // default 8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // default 40
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+// #define IGNORE_MOD_TAP_INTERRUPT
