bdf516cd8817300c92c24d3fe894fd0581954625
newtonapple <david.github@gmail.com>
2019-03-24 06:33:27 -0700

    Adding mouse button 3-5. More mouse speed tweaks.

--- users/newtonapple/config.h
+++ users/newtonapple/config.h
@@ -8,10 +8,16 @@
 // #define MOUSEKEY_WHEEL_MAX_SPEED   18  // default 8
 // #define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // default 40
 
-#define MOUSEKEY_DELAY              0  // default 300
-#define MOUSEKEY_INTERVAL          16  // default 50
-#define MOUSEKEY_MAX_SPEED         20  // default 10
-#define MOUSEKEY_TIME_TO_MAX       85  // default 20
+// #define MOUSEKEY_DELAY              0  // default 300
+// #define MOUSEKEY_INTERVAL          16  // default 50
+// #define MOUSEKEY_MAX_SPEED         20  // default 10
+// #define MOUSEKEY_TIME_TO_MAX       85  // default 20
+
+#define MOUSEKEY_DELAY              15  // default 300
+#define MOUSEKEY_INTERVAL           10  // default 50
+#define MOUSEKEY_MAX_SPEED          15  // default 10
+#define MOUSEKEY_TIME_TO_MAX        50  // default 20
+
 // #define MOUSEKEY_WHEEL_DELAY        0
 #define MOUSEKEY_WHEEL_MAX_SPEED   15  // default 8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // default 40
