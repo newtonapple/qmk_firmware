f3436be188a461a76b1332246a6c285aedc963a2
newtonapple <david.github@gmail.com>
2019-03-24 06:33:27 -0700

    Adding mouse button 3-5. More mouse speed tweaks.

--- keyboards/xd75/keymaps/newtonapple/keymap.c
+++ keyboards/xd75/keymaps/newtonapple/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, QUIT,    CLOSE,   KC_ACL2, REDO,    KC_PGUP, _______, KC_WH_U,  _______, COPY,    UNDO,    PREV_TAB, NEXT_TAB, PASTE,   CUT,
 	DEFAULT, SEL_ALL, KC_ACL2, KC_ACL0, KC_ACL1, KC_PGDN, KC_WH_L, KC_WH_D,  KC_WH_R, KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
 	_______, _______, _______, _______, KC_LSFT, _______, KC_BTN1, KC_MS_UP, KC_BTN2, KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R,  _______, _______,
-	_______, _______, _______, _______, KC_LSFT, KC_ACL0, KC_MS_L, KC_MS_D,  KC_MS_R, KC_BTN1, KC_BTN1, KC_BTN2,  KC_BTN3,  _______, _______
+	_______, _______, _______, _______, KC_LSFT, KC_ACL0, KC_MS_L, KC_MS_D,  KC_MS_R, KC_BTN1, KC_BTN1, KC_BTN2,  KC_BTN3,  KC_BTN4, KC_BTN5
  ),
 
 /* NUMBERS
