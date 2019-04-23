dda73c26e7e3b5e51ab474532a7acbe3d2a283ce
newtonapple <david.github@gmail.com>
2019-04-23 04:55:01 -0700

    Unify keymaps with wrapper templates.

--- keyboards/preonic/keymaps/newtonapple/keymap.c
+++ keyboards/preonic/keymaps/newtonapple/keymap.c
@@ -27,39 +27,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ________________ORTHO_GRID_QWERTY_L4________________, MACVIM_SQUOT, SFT_T_QUOT, NUM_MINS, GUI_T_RBRC, ADJ_BSLS, CRTL_SRBRC
   ),
 
-  [_CAP] = LAYOUT_preonic_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), _______,
-    DEFAULT, S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G), S(KC_H), S(KC_J), S(KC_K), S(KC_L), _______, _______,
-    _______, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M), _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  [_CAP] = LAYOUT_ortho_5x12_wrapper(
+    ______________________BLANK_6_______________________, ______________________BLANK_6_______________________,
+    _________________ORTHO_GRID_CAP_L1__________________, _________________ORTHO_GRID_CAP_R1__________________,
+    _________________ORTHO_GRID_CAP_L2__________________, _________________ORTHO_GRID_CAP_R2__________________,
+    _________________ORTHO_GRID_CAP_L3__________________, _________________ORTHO_GRID_CAP_R3__________________,
+    ______________________BLANK_6_______________________, ______________________BLANK_6_______________________
   ),
 
-  [_MACVIM] = LAYOUT_preonic_grid(
-    VIM_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,      KC_6,      KC_7,     KC_8,      KC_9,    KC_0,    _______,
-    _______,   VIM_0,   VIM_W,   VIM_E,   REDO,     VIM_DOL,   VIM_Y,     UNDO,     VIM_I,     VIM_O,   PASTE,   CUT,
-    DEFAULT,   SEL_ALL, SAVE,    VIM_D,   CMD_LEFT, CMD_RGHT,  KC_LEFT,   KC_DOWN,  KC_UP,     KC_RGHT, VIM_0,   VIM_DOL,
-    TG_VIMNUM, UNDO,    VIM_X,   VIM_C,   VIM_V,    VIM_B,     PREV_TAB,  NEXT_TAB, UNINDENT,  INDENT,  _______, _______,
-    _______,   _______, _______, _______, KC_LSFT,  _______,   KC_ENTER,  KC_LSFT,  MO_VIMNUM, _______, _______, _______
+  [_MACVIM] = LAYOUT_ortho_5x12_wrapper(
+    ________________ORTHO_GRID_MACVIM_L0________________, ________________ORTHO_GRID_MACVIM_R0________________,
+    ________________ORTHO_GRID_MACVIM_L1________________, ________________ORTHO_GRID_MACVIM_L1________________,
+    ________________ORTHO_GRID_MACVIM_L2________________, ________________ORTHO_GRID_MACVIM_R2________________,
+    ________________ORTHO_GRID_MACVIM_L3________________, ________________ORTHO_GRID_MACVIM_R3________________,
+    ________________ORTHO_GRID_MACVIM_L4________________, ________________ORTHO_GRID_MACVIM_R4________________
   ),
 
-  [_VIMNUM] = LAYOUT_preonic_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    DEFAULT, KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_6,    KC_7,   KC_8,     KC_9,    KC_0,    _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  [_VIMNUM] = LAYOUT_ortho_5x12_wrapper(
+	________________ORTHO_GRID_VIMNUM_L0________________, ________________ORTHO_GRID_VIMNUM_R0________________,
+	________________ORTHO_GRID_VIMNUM_L1________________, ________________ORTHO_GRID_VIMNUM_R1________________,
+	________________ORTHO_GRID_VIMNUM_L2________________, ________________ORTHO_GRID_VIMNUM_R2________________,
+	________________ORTHO_GRID_VIMNUM_L3________________, ________________ORTHO_GRID_VIMNUM_R3________________,
+	________________ORTHO_GRID_VIMNUM_L4________________, ________________ORTHO_GRID_VIMNUM_R4________________
   ),
 
-  [_MOUSE] = LAYOUT_preonic_grid(
-    _______, KC_ACL2, KC_ACL0, KC_ACL1, _______, _______, _______,  _______, _______,  _______,  _______, _______,
-    _______, QUIT,    CLOSE,   KC_ACL2, REDO,    KC_PGUP, COPY,     UNDO,    PREV_TAB, NEXT_TAB, PASTE,   CUT,
-    DEFAULT, SEL_ALL, KC_ACL2, KC_ACL0, KC_ACL1, KC_PGDN, KC_MS_L,  KC_MS_D, KC_MS_U,  KC_MS_R,  COPY,    PASTE,
-    _______, _______, _______, _______, KC_LSFT, _______, KC_WH_L,  KC_WH_D, KC_WH_U,  KC_WH_R,  KC_UP,   KC_UP,
-    _______, _______, _______, _______, KC_LSFT, KC_ACL1, KC_ENTER, KC_BTN1, KC_BTN2,  KC_LEFT,  KC_DOWN, KC_RGHT
+  [_MOUSE] = LAYOUT_ortho_5x12_wrapper(
+    ________________ORTHO_GRID_MOUSE_L0_________________, ________________ORTHO_GRID_MOUSE_R0_________________,
+    ________________ORTHO_GRID_MOUSE_L1_________________, ________________ORTHO_GRID_MOUSE_R1_________________,
+    ________________ORTHO_GRID_MOUSE_L2_________________, ________________ORTHO_GRID_MOUSE_R2_________________,
+    ________________ORTHO_GRID_MOUSE_L3_________________, ________________ORTHO_GRID_MOUSE_R3_________________,
+    ________________ORTHO_GRID_MOUSE_L4_________________, ________________ORTHO_GRID_MOUSE_R4_________________
   ),
 
-  [_NUM] = LAYOUT_preonic_grid(
+  [_NUM] = LAYOUT_ortho_5x12_wrapper(
     KC_BRIU, KC_BRID, C(KC_UP),  C(KC_DOWN), C(KC_LEFT), C(KC_RGHT), KC_MRWD,  KC_MPLY, KC_MFFD,    KC_MUTE, KC_VOLD,    KC_VOLU,
     _______, KC_GRV,  S(KC_GRV), KC_EQL,     KC_MINS,    S(KC_EQL),  S(KC_6),  S(KC_8), S(KC_BSLS), KC_BSLS, S(KC_MINS), _______,
     DEFAULT, KC_1,    KC_2,      KC_3,       KC_4,       KC_5,       KC_6,     KC_7,    KC_8,       KC_9,    KC_0,       _______,
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,   _______,    KC_LSFT,    _______,    _______,  OSL_SYM, KC_UP,      KC_DOWN, KC_LEFT,    KC_RGHT
   ),
 
-  [_SYM] = LAYOUT_preonic_grid(
+  [_SYM] = LAYOUT_ortho_5x12_wrapper(
     KC_F1,   KC_F2,   KC_F3,     KC_F4,   KC_F5,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,  KC_F11,     KC_F12 ,
     NUM,     KC_GRV,  S(KC_GRV), KC_EQL,  KC_MINS,  S(KC_EQL),  S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), KC_BSLS, S(KC_MINS), _______,
     CAP,     S(KC_1), S(KC_2),   S(KC_3), S(KC_4),  S(KC_5),    S(KC_6),    S(KC_7),    S(KC_8),    S(KC_9), S(KC_0),    _______,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,   _______, _______,   _______,   KC_BSPC,    S(KC_QUOT), S(KC_MINS), _______, _______,    _______
   ),
 
-  [_ADJUST] = LAYOUT_preonic_grid(
+  [_ADJUST] = LAYOUT_ortho_5x12_wrapper(
     KC_BRIU, KC_BRID, C(KC_UP), C(KC_DOWN), C(KC_LEFT), C(KC_RGHT), KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
     _______, MI_ON,   MI_OFF,   MU_MOD,     MUV_IN,     MUV_DE,     _______, _______, _______, _______, _______, RESET,
     _______, AU_ON,   AU_OFF,   CK_RST,     CK_UP,      CK_DOWN,    _______, _______, _______, _______, _______, _______,
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,  _______,    _______,    _______,    _______, _______, _______, _______, _______, AUDIO
   ),
 
-  [_AUDIO] = LAYOUT_preonic_grid(
+  [_AUDIO] = LAYOUT_ortho_5x12_wrapper(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     DEFAULT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
