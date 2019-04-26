237cad1f713a9a8e5e03f3a578d2719252b8968d
newtonapple <david.github@gmail.com>
2019-04-26 03:29:58 -0700

    Unify _NUM layer.

--- keyboards/xd75/keymaps/newtonapple/keymap.c
+++ keyboards/xd75/keymaps/newtonapple/keymap.c
@@ -34,19 +34,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT_ortho_5x15_wrapper(
-	 ________________ORTHO_GRID_QWERTY_L0________________, DF(_CODE),  SNAP_MAX,    INDENT,     ________________ORTHO_GRID_QWERTY_R0________________,
-	 ________________ORTHO_GRID_QWERTY_L1________________, PREV_TAB,   SNAP_CENTER, NEXT_TAB,   ________________ORTHO_GRID_QWERTY_R1________________,
-	 ________________ORTHO_GRID_QWERTY_L2________________, KC_HOME,    SNAP_BACK,   KC_PGUP,    ________________ORTHO_GRID_QWERTY_R2________________,
-	 ________________ORTHO_GRID_QWERTY_L3________________, KC_END,     KC_UP,       KC_PGDN,    ________________ORTHO_GRID_QWERTY_R3________________,
-	 ________________ORTHO_GRID_QWERTY_L4________________, MOUSE_LEFT, KC_DOWN,     CTL_T_RGHT, ________________ORTHO_GRID_QWERTY_R4________________
+     ________________ORTHO_GRID_QWERTY_L0________________, DF(_CODE),  SNAP_MAX,    INDENT,     ________________ORTHO_GRID_QWERTY_R0________________,
+     ________________ORTHO_GRID_QWERTY_L1________________, PREV_TAB,   SNAP_CENTER, NEXT_TAB,   ________________ORTHO_GRID_QWERTY_R1________________,
+     ________________ORTHO_GRID_QWERTY_L2________________, KC_HOME,    SNAP_BACK,   KC_PGUP,    ________________ORTHO_GRID_QWERTY_R2________________,
+     ________________ORTHO_GRID_QWERTY_L3________________, KC_END,     KC_UP,       KC_PGDN,    ________________ORTHO_GRID_QWERTY_R3________________,
+     ________________ORTHO_GRID_QWERTY_L4________________, MOUSE_LEFT, KC_DOWN,     CTL_T_RGHT, ________________ORTHO_GRID_QWERTY_R4________________
  ),
 
  [_CODE] = LAYOUT_ortho_5x15_wrapper(
-	 ________________ORTHO_GRID_QWERTY_L0________________, DF(_QWERTY), SNAP_MAX,    INDENT,     ________________ORTHO_GRID_QWERTY_R0________________,
-	 ________________ORTHO_GRID_QWERTY_L1________________, PREV_TAB,    SNAP_CENTER, NEXT_TAB,   ________________ORTHO_GRID_QWERTY_R1________________,
-	 ________________ORTHO_GRID_QWERTY_L2________________, KC_HOME,     SNAP_BACK,   KC_PGUP,    ________________ORTHO_GRID_QWERTY_R2________________,
-	 ________________ORTHO_GRID_QWERTY_L3________________, KC_END,      KC_UP,       KC_PGDN,    ________________ORTHO_GRID_QWERTY_R3________________,
-	 ________________ORTHO_GRID_QWERTY_L4________________, MOUSE_SPC,   KC_DOWN,     CTL_T_BSPC, ________________ORTHO_GRID_QWERTY_R4________________
+     ________________ORTHO_GRID_QWERTY_L0________________, DF(_QWERTY), SNAP_MAX,    INDENT,     ________________ORTHO_GRID_QWERTY_R0________________,
+     ________________ORTHO_GRID_QWERTY_L1________________, PREV_TAB,    SNAP_CENTER, NEXT_TAB,   ________________ORTHO_GRID_QWERTY_R1________________,
+     ________________ORTHO_GRID_QWERTY_L2________________, KC_HOME,     SNAP_BACK,   KC_PGUP,    ________________ORTHO_GRID_QWERTY_R2________________,
+     ________________ORTHO_GRID_QWERTY_L3________________, KC_END,      KC_UP,       KC_PGDN,    ________________ORTHO_GRID_QWERTY_R3________________,
+     ________________ORTHO_GRID_QWERTY_L4________________, MOUSE_SPC,   KC_DOWN,     CTL_T_BSPC, ________________ORTHO_GRID_QWERTY_R4________________
  ),
 
  [_CAP] = LAYOUT_ortho_5x15_wrapper(
@@ -72,19 +72,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_MACVIM] = LAYOUT_ortho_5x15_wrapper(
-	________________ORTHO_GRID_MACVIM_L0________________, _______,   QUIT,      CLOSE,       ________________ORTHO_GRID_MACVIM_R0________________,
-	________________ORTHO_GRID_MACVIM_L1________________, _______,   SNAP_MAX,  SNAP_CENTER, ________________ORTHO_GRID_MACVIM_R1________________,
-	________________ORTHO_GRID_MACVIM_L2________________, _______,   SNAP_BACK, _______,     ________________ORTHO_GRID_MACVIM_R2________________,
-	________________ORTHO_GRID_MACVIM_L3________________, _______,   SNAP_UP,   _______,     ________________ORTHO_GRID_MACVIM_R3________________,
-	________________ORTHO_GRID_MACVIM_L4________________, SNAP_LEFT, SNAP_DOWN, SNAP_RGHT,   ________________ORTHO_GRID_MACVIM_R4________________
+    ________________ORTHO_GRID_MACVIM_L0________________, _______,   QUIT,      CLOSE,       ________________ORTHO_GRID_MACVIM_R0________________,
+    ________________ORTHO_GRID_MACVIM_L1________________, _______,   SNAP_MAX,  SNAP_CENTER, ________________ORTHO_GRID_MACVIM_R1________________,
+    ________________ORTHO_GRID_MACVIM_L2________________, _______,   SNAP_BACK, _______,     ________________ORTHO_GRID_MACVIM_R2________________,
+    ________________ORTHO_GRID_MACVIM_L3________________, _______,   SNAP_UP,   _______,     ________________ORTHO_GRID_MACVIM_R3________________,
+    ________________ORTHO_GRID_MACVIM_L4________________, SNAP_LEFT, SNAP_DOWN, SNAP_RGHT,   ________________ORTHO_GRID_MACVIM_R4________________
  ),
 
  [_VIMNUM] = LAYOUT_ortho_5x15_wrapper(
-	________________ORTHO_GRID_VIMNUM_L0________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R0________________,
-	________________ORTHO_GRID_VIMNUM_L1________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R1________________,
-	________________ORTHO_GRID_VIMNUM_L2________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R2________________,
-	________________ORTHO_GRID_VIMNUM_L3________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R3________________,
-	________________ORTHO_GRID_VIMNUM_L4________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R4________________
+    ________________ORTHO_GRID_VIMNUM_L0________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R0________________,
+    ________________ORTHO_GRID_VIMNUM_L1________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R1________________,
+    ________________ORTHO_GRID_VIMNUM_L2________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R2________________,
+    ________________ORTHO_GRID_VIMNUM_L3________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R3________________,
+    ________________ORTHO_GRID_VIMNUM_L4________________, _________BLANK_3_________, ________________ORTHO_GRID_VIMNUM_R4________________
  ),
 
 
@@ -102,16 +102,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
  */
  [_MOUSE] = LAYOUT_ortho_5x15_wrapper(
-	________________ORTHO_GRID_MOUSE_L0_________________, _______, _______,  _______, ________________ORTHO_GRID_MOUSE_R0_________________,
-	________________ORTHO_GRID_MOUSE_L1_________________, _______, KC_WH_U,  _______, ________________ORTHO_GRID_MOUSE_R1_________________,
-	________________ORTHO_GRID_MOUSE_L2_________________, KC_WH_L, KC_WH_D,  KC_WH_R, ________________ORTHO_GRID_MOUSE_R2_________________,
-	________________ORTHO_GRID_MOUSE_L3_________________, KC_BTN1, KC_MS_UP, KC_BTN2, ________________ORTHO_GRID_MOUSE_R3_________________,
-	________________ORTHO_GRID_MOUSE_L4_________________, KC_MS_L, KC_MS_D,  KC_MS_R, ________________ORTHO_GRID_MOUSE_R4_________________
+    ________________ORTHO_GRID_MOUSE_L0_________________, _______, _______,  _______, ________________ORTHO_GRID_MOUSE_R0_________________,
+    ________________ORTHO_GRID_MOUSE_L1_________________, _______, KC_WH_U,  _______, ________________ORTHO_GRID_MOUSE_R1_________________,
+    ________________ORTHO_GRID_MOUSE_L2_________________, KC_WH_L, KC_WH_D,  KC_WH_R, ________________ORTHO_GRID_MOUSE_R2_________________,
+    ________________ORTHO_GRID_MOUSE_L3_________________, KC_BTN1, KC_MS_UP, KC_BTN2, ________________ORTHO_GRID_MOUSE_R3_________________,
+    ________________ORTHO_GRID_MOUSE_L4_________________, KC_MS_L, KC_MS_D,  KC_MS_R, ________________ORTHO_GRID_MOUSE_R4_________________
  ),
 
 /* NUMBERS
  * .---------------------------------------------------------------------------------------------------------------------------------------.
- * | F1      | F2     | F3     | F4     | F5     | F6     |        | SLEEP  | LED    | F7     | F8     | F9     | F10    | F11    | F12    |
+ * | F1      | F2     | F3     | F4     | F5     | F6     |        | SLEEP  | ADJUST | F7     | F8     | F9     | F10    | F11    | F12    |
  * |---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |         |        | `      | =      | -      | +      | P7     | P8     | P9     |        | [      | \      | ]      | _      |        |
  * |---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -124,11 +124,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_NUM] = LAYOUT_ortho_5x15_wrapper(
-	KC_F1,   KC_F2,   KC_F3,     KC_F4,   KC_F5,     KC_F6,      _______, SLEEP,   TG(_LED), KC_F7,      KC_F8,      KC_F9,      KC_F10,  KC_F11,     KC_F12 ,
-	_______, KC_GRV,  S(KC_GRV), KC_EQL,  KC_MINS,   S(KC_EQL),  KC_P7,   KC_P8,   KC_P9,    S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), KC_BSLS, S(KC_MINS), _______,
-	DEFAULT, KC_1,    KC_2,      KC_3,    KC_4,      KC_5,       KC_P4,   KC_P5,   KC_P6,    KC_6,       KC_7,       KC_8,       KC_9,    KC_0,       _______,
-	_______, _______, MOUSE,     MACVIM,  KC_QUOT,   S(KC_QUOT), KC_P1,   KC_P2,   KC_P3,    KC_LBRC,    KC_RBRC,    _______,    _______, _______,    _______,
-	_______, _______, _______,   _______, KC_LSFT,   _______,    KC_P0,   KC_PDOT, KC_ENT,   OSL_SYM,    _______,    _______,    _______, _______,    RESET
+    _________________ORTHO_GRID_NUM_L0__________________, _______, SLEEP,   TG(_ADJUST), _________________ORTHO_GRID_NUM_R0__________________,
+    _________________ORTHO_GRID_NUM_L1__________________, KC_P7,   KC_P8,   KC_P9,       _________________ORTHO_GRID_NUM_R1__________________,
+    _________________ORTHO_GRID_NUM_L2__________________, KC_P4,   KC_P5,   KC_P6,       _________________ORTHO_GRID_NUM_R2__________________,
+    _________________ORTHO_GRID_NUM_L3__________________, KC_P1,   KC_P2,   KC_P3,       _________________ORTHO_GRID_NUM_R3__________________,
+    _________________ORTHO_GRID_NUM_L4__________________, KC_P0,   KC_PDOT, KC_ENT,      _________________ORTHO_GRID_NUM_R4__________________
  ),
 
 /* SYMBOLS
@@ -146,19 +146,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_SYM] = LAYOUT_ortho_5x15_wrapper(
-	KC_F1,   KC_F2,    KC_F3,     KC_F4,   KC_F5,      KC_F6,      _______, SLEEP,   TG(_LED), KC_F7,      KC_F8,      KC_F9,      KC_F10,  KC_F11,     KC_F12 ,
-	NUM,     KC_GRV,   S(KC_GRV), KC_EQL,  KC_MINS,    S(KC_EQL),  KC_P7,   KC_P8,   KC_P9,    S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), KC_BSLS, S(KC_MINS), _______,
-	CAP,     S(KC_1),  S(KC_2),   S(KC_3), S(KC_4),    S(KC_5),    KC_P4,   KC_P5,   KC_P6,    S(KC_6),    S(KC_7),    S(KC_8),    S(KC_9), S(KC_0),    _______,
-	_______, _______,  MOUSE,     MACVIM,  KC_QUOT,    S(KC_QUOT), KC_P1,   KC_P2,   KC_P3,    KC_LBRC,    KC_RBRC,    _______,    _______, _______,    _______,
-	_______, _______,  _______,   _______, _______,    _______,    KC_P0,   KC_PDOT, KC_ENT,   KC_BSPC,    S(KC_QUOT), S(KC_MINS), _______, _______,    _______
+    KC_F1,   KC_F2,    KC_F3,     KC_F4,   KC_F5,      KC_F6,      _______, SLEEP,   TG(_ADJUST), KC_F7,      KC_F8,      KC_F9,      KC_F10,  KC_F11,     KC_F12 ,
+    NUM,     KC_GRV,   S(KC_GRV), KC_EQL,  KC_MINS,    S(KC_EQL),  KC_P7,   KC_P8,   KC_P9,       S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), KC_BSLS, S(KC_MINS), _______,
+    CAP,     S(KC_1),  S(KC_2),   S(KC_3), S(KC_4),    S(KC_5),    KC_P4,   KC_P5,   KC_P6,       S(KC_6),    S(KC_7),    S(KC_8),    S(KC_9), S(KC_0),    _______,
+    _______, _______,  MOUSE,     MACVIM,  KC_QUOT,    S(KC_QUOT), KC_P1,   KC_P2,   KC_P3,       KC_LBRC,    KC_RBRC,    _______,    _______, _______,    _______,
+    _______, _______,  _______,   _______, _______,    _______,    KC_P0,   KC_PDOT, KC_ENT,      KC_BSPC,    S(KC_QUOT), S(KC_MINS), _______, _______,    _______
  ),
 
- [_LED] = LAYOUT_ortho_5x15_wrapper(
-	RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, _______,  _______, TG(_LED), RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, TG(_LED),
-	_______, BL_TOGG, BL_OFF,  BL_ON,   _______,  _______,  RGB_RMOD, _______, RGB_MOD,  _______, _______, _______, _______, _______, _______,
-	DEFAULT, BL_BRTG, BL_DEC,  BL_INC,  _______,  _______,  RGB_SAI,  RGB_TOG, RGB_HUI,  _______, _______, _______, _______, _______, _______,
-	_______, _______, _______, _______, VLK_TOG,  _______,  RGB_SAD,  RGB_VAI, RGB_HUD,  _______, _______, _______, _______, _______, _______,
-	_______, _______, _______, _______, _______,  _______,  RGB_SPD,  RGB_VAD, RGB_SPI,  _______, _______, _______, _______, _______, _______
+ [_ADJUST] = LAYOUT_ortho_5x15_wrapper(
+    RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, _______,  _______, TG(_ADJUST), RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, TG(_ADJUST),
+    _______, BL_TOGG, BL_OFF,  BL_ON,   _______,  _______,  RGB_RMOD, _______, RGB_MOD,     _______, _______, _______, _______, _______, RESET,
+    DEFAULT, BL_BRTG, BL_DEC,  BL_INC,  _______,  _______,  RGB_SAI,  RGB_TOG, RGB_HUI,     _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, VLK_TOG,  _______,  RGB_SAD,  RGB_VAI, RGB_HUD,     _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,  _______,  RGB_SPD,  RGB_VAD, RGB_SPI,     _______, _______, _______, _______, _______, _______
  )
 };
 
@@ -184,6 +184,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t space_cadet_timer;
   SPACE_CADET_LAYER(keycode, MACVIM_SQUOT, _MACVIM, "\"", record->event.pressed);
   SPACE_CADET_LAYER(keycode, SYM_SMINS, _SYM, "_", record->event.pressed);
+  SPACE_CADET_LAYER(keycode, ADJ_SBSLS, _ADJUST, "|", record->event.pressed);
   SPACE_CADET(keycode, CRTL_SLBRC, KC_LCTRL, "{", record->event.pressed);
   SPACE_CADET(keycode, CRTL_SRBRC, KC_RCTRL, "}", record->event.pressed);
   SPACE_CADET(keycode, CMD_SMINS, KC_RCMD, "_", record->event.pressed);
