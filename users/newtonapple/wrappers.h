db10d38338411be198a6d343c5dad07f562f56f3
newtonapple <david.github@gmail.com>
2020-01-31 01:30:40 -0800

    Layout tweak: LT layer mods for z x , .

--- users/newtonapple/wrappers.h
+++ users/newtonapple/wrappers.h
@@ -40,11 +40,11 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
  */
 #define _________________QWERTY_L1_________________     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #define _________________QWERTY_L2_________________     KC_A,    KC_S,    KC_D,    KC_F,    KC_G
-#define _________________QWERTY_L3_________________     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+#define _________________QWERTY_L3_________________     NUM_Z,   SYM_X,    KC_C,    KC_V,    KC_B
 
-#define _________________QWERTY_R1_________________     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
-#define _________________QWERTY_R2_________________     KC_H,    KC_J,    KC_K,    KC_L,    SYM_SCLN
-#define _________________QWERTY_R3_________________     KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T_SLSH
+#define _________________QWERTY_R1_________________     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P
+#define _________________QWERTY_R2_________________     KC_H,    KC_J,    KC_K,     KC_L,     SYM_SCLN
+#define _________________QWERTY_R3_________________     KC_N,    KC_M,    SYM_COMM, NUM_DOT,  RSFT_T_SLSH
 
 
 #define ________________NUMBER_LEFT________________     KC_1,    KC_2,    KC_3,    KC_4,    KC_5
