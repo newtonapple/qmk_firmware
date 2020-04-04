5e6e21ce5ca2cd3c4c7660e4a2f5cece27c9c15e
newtonapple <david.github@gmail.com>
2020-04-03 21:52:38 -0700

    Commenting out Prime_E underglow for now.

---
+++ keyboards/primekb/prime_e/keymaps/newtonapple/config.h
@@ -0,0 +1,8 @@
+#pragma once
+// #define RGB_DI_PIN D7
+// #define RGBLIGHT_ANIMATIONS
+// // #define RGBLED_NUM 58
+// #define RGBLED_NUM 116
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
