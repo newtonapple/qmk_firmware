db548328529e0464ebb0fea80af6a452910cfcae
David Dai <david.github@gmail.com>
2020-12-27 20:33:59 -0800

    Add custom TAPPING_TERM_PER_KEY.

--- users/newtonapple/process_records.h
+++ users/newtonapple/process_records.h
@@ -91,4 +91,5 @@ enum custom_keycodes {
 // use these in keymap instad of their _user variants
 bool     process_record_keymap(uint16_t keycode, keyrecord_t *record);
 uint32_t layer_state_set_keymap(uint32_t state);
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record);
 #endif
