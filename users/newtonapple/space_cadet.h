7cd2eaf61e6f17521cc1e58be6f60cccf9fbdd20
David Dai <david.github@gmail.com>
2021-04-09 00:01:06 -0700

    Add projectkb/alice/newtonapple-split-bs. Add CRTL_SNAPBACK. Fix layer LED indicators.

--- users/newtonapple/space_cadet.h
+++ users/newtonapple/space_cadet.h
@@ -25,5 +25,4 @@
     };
 
 void space_cadet(uint16_t kc_mod, const char *str, bool pressed, uint16_t *timer, uint16_t timeout);
-
 void space_cadet_layer(uint8_t layer, const char *str, bool pressed, uint16_t *timer, uint16_t timeout);
